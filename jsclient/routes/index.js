cc1906cc4862398c8f931dea1d75fa7d90cf199e
Rover Chen <rover@Rover-Ma.local>
2019-11-13 09:59:29 +0800

    update balance

--- jsclient/routes/index.js
+++ jsclient/routes/index.js
@@ -44,11 +44,19 @@ router.get('/transfer',function(req, res){
 //Get Balance
 router.get('/balance', function(req, res){
     var userId = req.body.userId;
-    console.log(userId);
+    console.log('userId' + userId);
     var client = new dgcWalletClient(userId);
     var getYourBalance = client.balance();
     console.log(getYourBalance);
-    getYourBalance.then(result => {res.send({ balance: result, message:"Amount " + result + " available"});});
+    //getYourBalance.then(result => {res.send({ balance: result, message:"Amount " + result + " available"});});
+    getYourBalance.then(result => {
+        'userId didnot define!';
+        /*
+        res.send({ 
+            balance: result, 
+            message:"Amount " + result + " available"
+        });*/
+    });
 })
 
 //recieve data from login page and save it.
