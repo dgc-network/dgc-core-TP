e6b228d559515afc505a799c11c5cf845585d21d
Rover Chen <rover@Rover-Ma.local>
2019-11-23 18:10:45 +0800

    /project/dgc-wallet

---
+++ RESTapi/routes/dgcWalletRequest.js
@@ -0,0 +1,192 @@
+// Copyright (c) The dgc.network
+// SPDX-License-Identifier: Apache-2.0
+
+const {createHash} = require('crypto')
+const {CryptoFactory, createContext } = require('sawtooth-sdk/signing')
+const protobuf = require('sawtooth-sdk/protobuf')
+const fs = require('fs')
+const fetch = require('node-fetch');
+const {TextEncoder, TextDecoder} = require('text-encoding/lib/encoding')
+
+FAMILY_NAME='dgc-wallet'
+
+function hash(v) {
+  return createHash('sha512').update(v).digest('hex');
+}
+
+/**
+ * Generates a new private key, saving it to memory and storage (encrypted).
+ * Returns both a public key and the encrypted private key.
+ */
+const {Secp256k1PrivateKey} = require('sawtooth-sdk/signing/secp256k1')	
+const secp256k1 = require('sawtooth-sdk/signing/secp256k1')
+const context = new secp256k1.Secp256k1Context()
+//const context = createContext('secp256k1');
+//let privateKey = null
+//let publicKey = null
+//let encryptedKey = null
+
+
+class dgcWalletRequest {
+  //constructor(userid) {
+    //const privateKeyStrBuf = this.getUserPriKey(userid);
+    //const privateKeyStr = privateKeyStrBuf.toString().trim();
+  constructor(privateKeyStr) {
+    if (null == privateKeyStr)
+      console.log("privateKey is empty");
+    else {
+      const privateKey = Secp256k1PrivateKey.fromHex(privateKeyStr);
+      this.signer = new CryptoFactory(context).newSigner(privateKey);
+      this.publicKey = this.signer.getPublicKey().asHex();
+      this.address = hash("dgc-wallet").substr(0, 6) + hash(this.publicKey).substr(0, 64);
+      console.log("Storing at: " + this.address);  
+    }
+  }
+
+  makePrivateKey() {
+    let privateKey = context.newRandomPrivateKey()
+    privateKey = privateKey.asHex()
+    return privateKey
+  }
+
+  getPublicKey() {
+    return this.publicKey
+  }
+
+  dgcBalance() {
+    let amount = this._send_to_rest_api(null);
+    return amount;
+  }
+
+  dgcTransfer(user2, amount) {
+    this._wrap_and_send("transfer", [amount, user2]);
+  }
+
+  buyDGC(user2, amount) {
+    this._wrap_and_send("buyDGC", [amount, user2]);
+  }
+
+  sellDGC(user2, amount) {
+    this._wrap_and_send("sellDGC", [amount, user2]);
+  }
+
+/*
+  deposit(amount) {
+    this._wrap_and_send("deposit", [amount]);
+  }
+
+  withdraw(amount) {
+    this._wrap_and_send("withdraw", [amount]);
+  }	
+
+  balance() {
+    let amount = this._send_to_rest_api(null);
+    return amount;
+  }
+
+  transfer(user2, amount) {
+    this._wrap_and_send("transfer", [amount, user2]);
+  }
+
+  getUserPriKey(userid) {
+    console.log(userid);
+    console.log("Current working directory is: " + process.cwd());
+    var userprivkeyfile = '/root/.sawtooth/keys/'+userid+'.priv';
+    return fs.readFileSync(userprivkeyfile);
+  }	
+
+  getUserPubKey(userid) {
+    console.log(userid);
+    console.log("Current working directory is: " + process.cwd());
+    var userpubkeyfile = '/root/.sawtooth/keys/'+userid+'.pub';
+    return fs.readFileSync(userpubkeyfile);
+  }
+*/
+  _wrap_and_send(action,values){
+    var payload = ''
+    const address = this.address;
+    console.log("wrapping for: " + this.address);
+    var inputAddressList = [address];
+    var outputAddressList = [address];
+    if (action === "transfer") {
+	    //const pubKeyStrBuf = this.getUserPubKey(values[1]);
+      //const pubKeyStr = pubKeyStrBuf.toString().trim();
+      console.log(values[1]);
+	    const pubKeyStr = values[1];
+      var toAddress = hash("dgc-wallet").substr(0, 6) + hash(pubKeyStr).substr(0, 64);
+      inputAddressList.push(toAddress);
+      outputAddressList.push(toAddress);
+      payload = action+","+values[0]+","+pubKeyStr;
+    } else {
+	    payload = action+","+values[0];
+    }	
+    var enc = new TextEncoder('utf8');
+    const payloadBytes = enc.encode(payload);
+    const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+      familyName: 'dgc-wallet',
+      familyVersion: '1.0',
+      inputs: inputAddressList,
+      outputs: outputAddressList,
+      signerPublicKey: this.signer.getPublicKey().asHex(),
+      nonce: "" + Math.random(),
+      batcherPublicKey: this.signer.getPublicKey().asHex(),
+      dependencies: [],
+      payloadSha512: hash(payloadBytes),
+    }).finish();
+    const transaction = protobuf.Transaction.create({
+      header: transactionHeaderBytes,
+      headerSignature: this.signer.sign(transactionHeaderBytes),
+      payload: payloadBytes
+    });
+    const transactions = [transaction];
+    const batchHeaderBytes = protobuf.BatchHeader.encode({
+      signerPublicKey: this.signer.getPublicKey().asHex(),
+      transactionIds: transactions.map((txn) => txn.headerSignature),
+    }).finish();
+    const batchSignature = this.signer.sign(batchHeaderBytes);
+    const batch = protobuf.Batch.create({
+      header: batchHeaderBytes,
+      headerSignature: batchSignature,
+      transactions: transactions,
+    });
+    const batchListBytes = protobuf.BatchList.encode({
+      batches: [batch]
+    }).finish();
+    this._send_to_rest_api(batchListBytes);	
+  }
+
+  _send_to_rest_api(batchListBytes){
+    if (batchListBytes == null) {
+      var geturl = 'http://rest-api:8008/state/'+this.address
+      console.log("Getting from: " + geturl);
+      return fetch(geturl, {
+        method: 'GET',
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        var data = responseJson.data;
+        var amount = new Buffer(data, 'base64').toString();
+        return amount;
+      })
+      .catch((error) => {
+        console.error(error);
+      }); 	
+    } else {
+      fetch('http://rest-api:8008/batches', {
+ 	      method: 'POST',
+        headers: {
+	        'Content-Type': 'application/octet-stream'
+        },
+        body: batchListBytes
+	    })
+	    .then((response) => response.json())
+	    .then((responseJson) => {
+        console.log(responseJson);
+      })
+      .catch((error) => {
+ 	      console.error(error);
+      }); 	
+    }
+  }
+}
+module.exports.dgcWalletRequest = dgcWalletRequest;
---
+++ RESTapi/routes/index.js
@@ -0,0 +1,245 @@
+// Copyright (c) The dgc.network
+// SPDX-License-Identifier: Apache-2.0
+
+const _ = require('lodash')
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var {dgcWalletRequest} = require('./dgcWalletRequest') 
+//const auth = require('./auth')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+router.get('/', function(req, res){
+    //res.redirect("/login");
+    res.render('homePage');
+})
+
+//Get home view
+router.get('/login', function(req, res){
+    res.render('loginPage');
+});
+
+//Get main view
+router.get('/home', function(req, res){
+    res.render('homePage');
+});
+
+// Get Deposit view
+router.get('/deposit',function(req, res){
+    res.render('depositPage');
+})
+
+//Get Withdraw view
+router.get('/withdraw',function(req, res){
+    res.render('withdrawPage');
+})
+
+//Get Transfer View
+router.get('/transfer',function(req, res){
+    res.render('transferPage');
+})
+
+//Get Balance View
+router.get('/balance', function(req, res){
+    res.render('balancePage');
+})
+
+//recieve data from login page and save it.
+router.post('/login', urlencodedParser, function(req, res){
+    var userid = req.body.userId;
+    res.send({done:1, userId: userid, message: "User Successfully Logged in as "+userid  });
+});
+
+//function to deposit amount in server
+router.post('/deposit', function(req, res) {
+    //var userId = req.body.userId;
+    //var client = new dgcWalletRequest(userId); 
+    var amount = req.body.money;
+    var privateKey = req.body.privateKey;
+    var client = new dgcWalletRequest(privateKey);
+    client.deposit(amount);    
+    res.send({message:"Amount "+ amount +" successfully added"});
+});
+
+//function to withdraw
+router.post('/withdraw', function(req, res) {
+    //var userId = req.body.userId;
+    //var client = new dgcWalletRequest(userId);   
+    var amount = req.body.money;
+    var privateKey = req.body.privateKey;
+    var client = new dgcWalletRequest(privateKey);
+    client.withdraw(amount);     
+    res.send({  message:"Amount "+ amount +" successfully deducted"});
+});
+
+//function to transfer money to another user
+router.post('/transfer', function(req, res) {
+    //var userId = req.body.userId;
+    //var client = new dgcWalletRequest(userId);
+    var beneficiary = req.body.beneficiary;
+    var amount = req.body.money;
+    var privateKey = req.body.privateKey;
+    var client = new dgcWalletRequest(privateKey);
+    client.transfer(beneficiary, amount);    
+    res.send({ message:"Amount "+ amount +" successfully added to " + beneficiary});
+});
+
+router.post('/balance', function(req, res){
+    //var userId = req.body.userId;
+    //var client = new dgcWalletRequest(userId);
+    var privateKey = req.body.privateKey;
+    var client = new dgcWalletRequest(privateKey);
+    var getYourBalance = client.balance();
+    console.log(getYourBalance);
+    getYourBalance.then(result => {res.send({ balance: result, message:"Amount " + result + " available"});});
+})
+
+// Copyright (c) The dgc.network
+// makePrivateKey
+router.post('/makePrivateKey', function(req, res){
+    var client = new dgcWalletRequest(req.body.privateKey);
+    res.send({privateKey: client.makePrivateKey()});
+})
+
+// getPublicKey
+router.post('/getPublicKey', function(req, res){
+    if (null == req.body.privateKey) {
+        res.send({error: "privateKey is empty"});
+    } else {
+        var client = new dgcWalletRequest(req.body.privateKey);
+        res.send({publicKey: client.getPublicKey()});
+    }
+})
+
+// dgcBalance
+router.post('/dgcBalance', function(req, res){
+    if (null == req.body.privateKey) {
+        res.send({error: "privateKey is empty"});
+    } else {
+        var client = new dgcWalletRequest(req.body.privateKey);
+        var getBalance = client.dgcBalance();
+        getBalance.then(result => {
+            res.send({ balance: result, message:"Amount " + result + " available"});
+        });
+    }
+})
+
+// Transfer DGC to another user
+router.post('/dgcTransfer', function(req, res) {
+    if (null == req.body.privateKey) {
+        res.send({error: "privateKey is empty"});
+    } else if (null == req.body.beneficiary) {
+        res.send({error: "beneficiary is empty"});
+    } else {
+        var client = new dgcWalletRequest(req.body.privateKey);
+        var getBalance = client.dgcBalance();
+        getBalance.then(result => {
+            if (req.body.DGC > result ) {
+                res.send({ balance: result, message:"your DGC balance is not enough"});
+            } else {
+                var beneficiary = req.body.beneficiary;
+                var amount = req.body.DGC;
+                client.dgcTransfer(beneficiary, amount);
+                res.send({ message:"Amount "+ amount +" successfully added to " + beneficiary});        
+            }
+        });
+    }
+});
+
+// Buy DGC from marketplace
+router.post('/buyDGC', function(req, res) {
+    if (null == req.body.privateKey) {
+        res.send({error: "privateKey is empty"});
+    } else if (null == req.body.currency) {
+        res.send({error: "Currency cannot be empty"});
+    } else {
+        var client = new dgcWalletRequest(req.body.privateKey);
+        var getBalance = client.dgcBalance();
+        getBalance.then(result => {
+            if (req.body.DGC > result ) {
+                res.send({ balance: result, message:"your DGC balance is not enough"});
+            } else {
+                var beneficiary = req.body.beneficiary;
+                var amount = req.body.DGC;
+                client.dgcTransfer(beneficiary, amount);
+                res.send({ message:"Amount "+ amount +" successfully added to " + beneficiary});        
+            }
+        });
+    }
+});
+
+// sell DGC to marketplace
+router.post('/sellDGC', function(req, res) {
+    if (null == req.body.privateKey) {
+        res.send({error: "privateKey is empty"});
+    } else if (null == req.body.currency) {
+        res.send({error: "Currency cannot be empty"});
+    } else {
+        var client = new dgcWalletRequest(req.body.privateKey);
+        var getBalance = client.dgcBalance();
+        getBalance.then(result => {
+            if (req.body.DGC > result ) {
+                res.send({ balance: result, message:"your DGC balance is not enough"});
+            } else {
+                var sellAmount = req.body.DGC;
+                let buyingList = client.dgcBuyingList();
+                for (i = 0; i < buyingList.length; i++) {
+                    if (sellAmount > buyingList[i].DGC) {
+                        var beneficiary = buyingList[i].beneficiary;
+                        client.buyDGC(beneficiary, buyingList[i].DGC);
+                        sellAmount = sellAmount - buyingList[i].DGC;
+                    } else {
+
+                    }
+                }
+                res.send({ message:"Amount "+ amount +" successfully added to " + beneficiary});        
+            }
+        });
+    }
+});
+
+// Is Registered
+router.get('/isRegistered', function(req, res){
+    if (null == req.body.privateKey) {
+        res.send({message: 'You have not the privateKey'})
+    }
+})
+
+// Get Info
+router.get('/info', function(req, res){
+    var userId = req.body.userId;
+    res.send({
+        //pubkey: batcher.getPublicKey(),
+        //mapsApiKey: config.MAPS_API_KEY,
+        endpoints: endpointInfo
+    });
+})
+
+// Parses the endpoints from an Express router
+const getEndpoints = router => {
+    return _.chain(router.stack)
+    .filter(layer => layer.route)
+    .map(({ route }) => {
+        return _.chain(route.stack)
+        .reduceRight((layers, layer) => {
+            if (layer.name === 'restrict') {
+                _.nth(layers, -1).restricted = true
+            } else {
+                layers.push({
+                    path: route.path,
+                    method: layer.method.toUpperCase(),
+                    restricted: false
+                })
+            }
+            return layers
+        }, [])
+        .reverse()
+        .value()
+    })
+    .flatten()
+    .value()
+}
+const endpointInfo = getEndpoints(router)
+
+module.exports = router;
