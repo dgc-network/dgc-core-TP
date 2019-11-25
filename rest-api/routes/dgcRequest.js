014da20160bd8842be9ab89813b432e9e48c3a0f
roverchen <rover.k.chen@gmail.com>
2019-11-25 15:40:44 +0800

    var privateKey = new Buffer(privateKeyStr, 'hex');

--- rest-api/routes/dgcRequest.js
+++ rest-api/routes/dgcRequest.js
@@ -27,12 +27,9 @@ const context = new secp256k1.Secp256k1Context()
 
 class dgcRequest {
   constructor(privateKeyStr) {
-    const privateKey = Secp256k1PrivateKey.fromHex(privateKeyStr)
-    .catch((error) => {
-      console.error(error);
-      return false;
-    });
-  this.signer = new CryptoFactory(context).newSigner(privateKey);
+    //const privateKey = Secp256k1PrivateKey.fromHex(privateKeyStr);
+    var privateKey = new Buffer(privateKeyStr, 'hex');
+    this.signer = new CryptoFactory(context).newSigner(privateKey);
     this.publicKey = this.signer.getPublicKey().asHex();
     this.address = hash("dgc-core").substr(0, 6) + hash(this.publicKey).substr(0, 64);
     console.log("Storing at: " + this.address);  
